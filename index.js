f693762fe8eeb2d978085fe626775409b179f782
Simon Ong <61894953+ioctlsg@users.noreply.github.com>
2023-08-29 21:43:48 +0800

    add with post

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ module.exports.handler = async (event) => {
       ),
     };
   };
+
+  module.exports.handlerB = async (event) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: "Your function executed successfully!",
+          access_key: process.env.ACCESS_KEY
+        },
+        null,
+        2
+      ),
+    };
+  };
