8ce2dea3cc421dd20eb7335b2ff486807178ebf7
Simon Ong <61894953+ioctlsg@users.noreply.github.com>
2023-08-29 21:58:46 +0800

    add with post

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ module.exports.handler = async (event) => {
 
   module.exports.handlerB = async (event) => {
     return {
-      statusCode: 200,
+      statusCode: 300,
       body: JSON.stringify(
         {
           message: "Your function executed successfully!",
-          access_key: process.env.ACCESS_KEY
+          //access_key: process.env.ACCESS_KEY
         },
         null,
         2
